d46d83444564d09ba621439678e7f735484c6d8a
Oleksandr Kulchytskyi <sashaas@inbox.ru>
2013-01-17 00:49:14 +0200

    Add some stuff in unit test

--- Finder.test/UnitTest1.cs
+++ Finder.test/UnitTest1.cs
@@ -15,9 +15,10 @@ namespace Finder.test
 			IFileHasherFinder finder2 = new FileHasherFinder();
 			var task = finder.DoSearch(@"d:\ebooks", "*.*", true);
 			var task2 = finder2.DoSearch(@"D:\Dropbox\SkyDrive\eBooks", "*.*", true);
+			var task3 = finder.DoSearch(@"D:\downloads", "*.*", true);
 			try
 			{
-				System.Threading.Tasks.Task.WaitAll(task, task2);
+				System.Threading.Tasks.Task.WaitAll(task, task2, task3);
 			}
 			catch (AggregateException ex)
 			{
@@ -32,16 +33,27 @@ namespace Finder.test
 				Assert.IsNotNull(task.Result);
 				Assert.IsNotNull(task2.Result);
 
-				var list1 = task2.Result;
-				var list2 = task.Result;
+				var list1 = task.Result;
+				var list2 = task2.Result;
 				int count1 = list1.Count();
 				int count2 = list2.Count();
+				var list3 = task3.Result;
+
 				Assert.AreNotEqual(count1, count2);
 
 				var overall = (from item1 in list1
 							   from item2 in list2
 							   where item1.ShaCode.Equals(item2.ShaCode, StringComparison.OrdinalIgnoreCase)
 							   select new { Path1 = item2.PathToFile, Path2 = item1.PathToFile, }).ToList();
+				if (overall.Count > 0)
+				{
+
+				}
+
+				var overall2 = (from item1 in list2
+								from item2 in list3
+								where item1.ShaCode.Equals(item2.ShaCode, StringComparison.OrdinalIgnoreCase)
+								select new { Path1 = item2.PathToFile, Path2 = item1.PathToFile, }).ToList();
 				if (overall.Count > 0)
 				{
 
