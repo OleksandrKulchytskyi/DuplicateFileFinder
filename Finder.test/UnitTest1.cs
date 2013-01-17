7e4d6dc9b5954e37c14887bff244dba96ebc800c
Oleksandr Kulchytskyi <sashaas@inbox.ru>
2013-01-17 13:09:11 +0200

    -some changes

--- Finder.test/UnitTest1.cs
+++ Finder.test/UnitTest1.cs
@@ -13,54 +13,71 @@ namespace Finder.test
 		{
 			IFileHasherFinder finder = new FileHasherFinder();
 			IFileHasherFinder finder2 = new FileHasherFinder();
-			var task = finder.DoSearch(@"d:\ebooks", "*.*", true);
-			var task2 = finder2.DoSearch(@"D:\Dropbox\SkyDrive\eBooks", "*.*", true);
-			var task3 = finder.DoSearch(@"D:\downloads", "*.*", true);
-			try
+			var task = finder.DoSearch(@"d:\ebooks", "*.jpg", true);
+			//var task2 = finder2.DoSearch(@"D:\Dropbox\SkyDrive\eBooks", "*.*", true);
+			//var task3 = finder.DoSearch(@"D:\downloads", "*.*", true);
+
+			task.Wait();
+
+			if (!task.IsFaulted)
 			{
-				System.Threading.Tasks.Task.WaitAll(task, task2, task3);
-			}
-			catch (AggregateException ex)
-			{
-				System.Diagnostics.Debug.WriteLine(ex.InnerException.Message);
-			}
-			catch (Exception ex)
-			{
-				System.Diagnostics.Debug.WriteLine(ex.Message);
-			}
-			if ((!task.IsFaulted && !task.IsCanceled) && (!task2.IsFaulted && !task2.IsCanceled))
-			{
-				Assert.IsNotNull(task.Result);
-				Assert.IsNotNull(task2.Result);
+				int count = task.Result.Count();
+				var data = from item in task.Result
+							   group item by item.ShaCode into grouping
+							   orderby grouping.Key
+							   where grouping.Count() < 1
+							   select grouping;
 
-				var list1 = task.Result;
-				var list2 = task2.Result;
-				int count1 = list1.Count();
-				int count2 = list2.Count();
-				var list3 = task3.Result;
-
-				Assert.AreNotEqual(count1, count2);
-
-				var overall = (from item1 in list1
-							   from item2 in list2
-							   where item1.Equals(item2)
-							   select new { Path1 = item2.PathToFile, Path2 = item1.PathToFile, }).ToList();
-				if (overall.Count > 0)
-				{
-
-				}
-
-				var overall2 = (from item1 in list2
-								from item2 in list3
-								where item1.ShaCode.Equals(item2.ShaCode, StringComparison.OrdinalIgnoreCase)
-								select new { Path1 = item2.PathToFile, Path2 = item1.PathToFile, }).ToList();
-				if (overall.Count > 0)
+				if(data.ToList()!=null)
 				{
 
 				}
 			}
-			else
-				Assert.Fail();
+			//try
+			//{
+			//	System.Threading.Tasks.Task.WaitAll(task, task2, task3);
+			//}
+			//catch (AggregateException ex)
+			//{
+			//	System.Diagnostics.Debug.WriteLine(ex.InnerException.Message);
+			//}
+			//catch (Exception ex)
+			//{
+			//	System.Diagnostics.Debug.WriteLine(ex.Message);
+			//}
+			//if ((!task.IsFaulted && !task.IsCanceled) && (!task2.IsFaulted && !task2.IsCanceled))
+			//{
+			//	Assert.IsNotNull(task.Result);
+			//	Assert.IsNotNull(task2.Result);
+
+			//	var list1 = task.Result;
+			//	var list2 = task2.Result;
+			//	int count1 = list1.Count();
+			//	int count2 = list2.Count();
+			//	var list3 = task3.Result;
+
+			//	Assert.AreNotEqual(count1, count2);
+
+			//	var overall = (from item1 in list1
+			//				   from item2 in list2
+			//				   where item1.Equals(item2)
+			//				   select new { Path1 = item2.PathToFile, Path2 = item1.PathToFile, }).ToList();
+			//	if (overall.Count > 0)
+			//	{
+
+			//	}
+
+			//	var overall2 = (from item1 in list2
+			//					from item2 in list3
+			//					where item1.ShaCode.Equals(item2.ShaCode, StringComparison.OrdinalIgnoreCase)
+			//					select new { Path1 = item2.PathToFile, Path2 = item1.PathToFile, }).ToList();
+			//	if (overall.Count > 0)
+			//	{
+
+			//	}
+			//}
+			//else
+			//	Assert.Fail();
 		}
 	}
 }
