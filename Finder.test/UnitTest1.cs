13e0cf55a38bc41556d76824ad3b78ab4b7d93c8
Oleksandr Kulchytskyi <sashaas@inbox.ru>
2013-01-21 23:02:07 +0200

    some changes in unittest1.cs module

--- Finder.test/UnitTest1.cs
+++ Finder.test/UnitTest1.cs
@@ -12,12 +12,12 @@ namespace Finder.test
 		public void TestMethod1()
 		{
 			IFileHasherFinder finder = new FileHasherFinder();
-			//var task = finder.DoSearch(@"d:\ebooks", "*.pdf", true);
-			var task = finder.DoSearch(@"D:\eBooks", "*.pdf", true);
-			//var task3 = finder.DoSearch(@"D:\downloads", "*.*", true);
+			var task = finder.DoSearch(@"d:\ebooks", "*.pdf", true);
+			var task2 = finder.DoSearch(@"D:\Dropbox\SkyDrive\eBooks", "*.pdf", true);
+
 			try
 			{
-				System.Threading.Tasks.Task.WaitAll(task);//, task3);
+				System.Threading.Tasks.Task.WaitAll(task, task2);
 			}
 			catch (AggregateException ex)
 			{
@@ -32,9 +32,9 @@ namespace Finder.test
 				Assert.IsNotNull(task.Result);
 
 				var list1 = task.Result;
-				//var list2 = task2.Result;
+				var list2 = task2.Result;
 				int count1 = list1.Count();
-				//int count2 = list2.Count();
+				int count2 = list2.Count();
 				//var list3 = task3.Result;
 
 				var grouping = from item in list1
@@ -43,10 +43,28 @@ namespace Finder.test
 							   where groups.Count() > 1
 							   select groups;
 
-				if (grouping.ToList() != null)
-
+				var gpList1 = grouping.ToList();
+				if (gpList1 != null)
 				{
+				}
 
+				var grouping2 = from item in list2
+								group item by item.ShaCode into groups
+								orderby groups.Key
+								where groups.Count() > 1
+								select groups;
+
+				var gpList2 = grouping2.ToList();
+				if (gpList2 != null)
+				{
+				}
+
+				var duplicate = (from item in list1
+								from item2 in list2
+								where item.ShaCode.Equals(item2.ShaCode)
+								select new { SHA = item.ShaCode, PATH1 = item.PathToFile, PATH2 = item2.PathToFile }).ToList();
+				if(duplicate!=null)
+				{
 				}
 			}
 		}
